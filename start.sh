a8729e8382181b10ff5c7cc99b2398c4b4a86dd5
ji che <129924492+WFJ1987@users.noreply.github.com>
2023-09-27 11:11:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export NEZHA_SERVER=${NEZHA_SERVER:-''}
-export NEZHA_PORT=${NEZHA_PORT:-''}
-export NEZHA_KEY=${NEZHA_KEY:-''}
-export TLS=${TLS:-'1'}
+export NEZHA_SERVER='nezhedata.dreama.eu.org'
+export NEZHA_PORT='443'
+export NEZHA_KEY='TCP2OCmYeQ1DwiGT1i'
+export TLS='1'
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
 export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
 export ARGO_AUTH=${ARGO_AUTH:-''}
